85d793148856eb3033df6dc3089ac1109c4cff61
Jonathan Turner <jt@jtnet.co.uk>
2017-11-18 12:54:29 +0000

    ec2inst

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Small single purpose executables in the sytle of Unix CLI commands.
 
 * ec2inst - Query information about the EC2 instance the command is running on.
+
+## Building
+```
+go build -ldflags "-X main.buildstamp=`date -u '+%FT%T%Z'` -X main.githash=`git rev-parse HEAD`"
+```
--- ec2inst/main.go
+++ ec2inst/main.go
@@ -1,8 +1,9 @@
-package ec2inst
+package main
 
 import (
 	"flag"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
@@ -17,12 +18,11 @@ const (
 	region           = "region"
 	accountID        = "account"
 	availabilityZone = "az"
-	version          = "-v"
 	arch             = "arch"
 	billingProducts  = "billingproducts"
 	productCodes     = "productcodes"
 	imageID          = "imgid"
-	instanceType     = "instid"
+	instanceType     = "type"
 	kernelID         = "kernelid"
 	pendingTime      = "pending"
 	privateIP        = "pvtip"
@@ -40,7 +40,7 @@ func main() {
 	sess := session.Must(session.NewSession())
 	ec2md := ec2metadata.New(sess)
 	if !ec2md.Available() {
-		fmt.Fprintln(os.Stderr, "Instance metadata not available. May not be running on AWS.")
+		fmt.Fprintln(os.Stderr, "Instance metadata not available. Instance may not be running on AWS.")
 		os.Exit(1)
 	}
 	idoc, err := ec2md.GetInstanceIdentityDocument()
@@ -48,13 +48,14 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error getting instance identity document: %v\n", err)
 		os.Exit(1)
 	}
+	sess = session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(idoc.Region),
+	}))
 
 	flag.Parse()
 
-	var l string
+	var l []string
 	switch flag.Arg(0) {
-	case version:
-		versionInfo()
 	case accountID:
 		l = append(l, idoc.AccountID)
 	case region:
@@ -131,46 +132,44 @@ func main() {
 		usage()
 	}
 	for i := range l {
-		fmt.Println(l[i])
+		if l[i] != "" {
+			fmt.Println(l[i])
+		}
 	}
-	os.Exit(0)
-}
-
-func versionInfo() {
-	fmt.Printf("Build hash: %v\nBuild timestamp: %v\n", githash, buildstamp)
 	os.Exit(0)
 }
 
 func usage() {
 	fmt.Printf(`
 	Usage: %s [information type]
-		%s	Show version information for this utility.
-		%s	Show this instance's ID.
-		%s	Show the account ID in which this instance resides.
-		%s	Show the AWS region in which this instance resides.
-		%s	Show the availability zone in which this instance resides.
-		%s	Show the instance type.
-		%s	Show the instance's image ID.
-		%s	Show the instance architecture.
-		%s	Show the instance's kernel ID.
-		%s	Show the instance's pending time.
-		%s	Show the instance's product codes.
-		%s	Show the instance's billing products.
-		%s	Show the instance's state.
-		%s 	Show the instance's tags (<key>:<value>)
-		%s	Show the instance's EBS volumes (<device name>:<volume id>:<status>:<attach time>:<delete on termination>)
-		%s	Show the instance's private IP
-		%s	Show the instance's public IP
-		%s	Show the instance's public DNS name
-		%s	Show the instance's security groups (<group id>:<group name>)
-		%s	Show the instance's VPC ID
-		%s	Show the instance's subnet ID
+		%s		Show this instance's ID.
+		%s		Show the account ID in which this instance resides.
+		%s		Show the AWS region in which this instance resides.
+		%s		Show the availability zone in which this instance resides.
+		%s		Show the instance type.
+		%s		Show the instance's image ID.
+		%s		Show the instance architecture.
+		%s		Show the instance's kernel ID.
+		%s		Show the instance's pending time.
+		%s		Show the instance's product codes.
+		%s		Show the instance's billing products.
+		%s		Show the instance's state.
+		%s 		Show the instance's tags (<key>:<value>)
+		%s		Show the instance's EBS volumes (<device name>:<volume id>:<status>:<attach time>:<delete on termination>)
+		%s		Show the instance's private IP
+		%s		Show the instance's public IP
+		%s		Show the instance's public DNS name
+		%s		Show the instance's security groups (<group id>:<group name>)
+		%s		Show the instance's VPC ID
+		%s		Show the instance's subnet ID
 
 	References:
 		- http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
-	`,
+	Version:
+		Build hash: %v
+		Build timestamp: %v
+`,
 		os.Args[0],
-		version,
 		instanceID,
 		accountID,
 		region, availabilityZone,
@@ -180,15 +179,16 @@ func usage() {
 		tags, ebsVols,
 		privateIP, publicIP, publicDNS,
 		securityGroups, vpcID, subnetID,
+		githash, buildstamp,
 	)
 	os.Exit(0)
 }
 
 func describeInstance(sess *session.Session, instID string) (*ec2.DescribeInstancesOutput, error) {
 	var input ec2.DescribeInstancesInput
-	input.SetInstanceIds([]string{instID})
+	input.SetInstanceIds([]*string{&instID})
 	ec2Svc := ec2.New(sess)
-	return ec2Svc.DescribeInstances(input)
+	return ec2Svc.DescribeInstances(&input)
 }
 
 func getTags(output *ec2.DescribeInstancesOutput) (l []string) {
@@ -207,7 +207,7 @@ func getSGs(output *ec2.DescribeInstancesOutput) (l []string) {
 
 func getEBS(output *ec2.DescribeInstancesOutput) (l []string) {
 	for _, vol := range output.Reservations[0].Instances[0].BlockDeviceMappings {
-		s := fmt.Sprintf("%s:%s:%s:%v:%t", vol.DeviceName, vol.Ebs.VolumeId, vol.Ebs.Status, vol.Ebs.AttachTime, vol.Ebs.DeleteOnTermination)
+		s := fmt.Sprintf("%s:%s:%s:%v:%t", *vol.DeviceName, *vol.Ebs.VolumeId, *vol.Ebs.Status, vol.Ebs.AttachTime, *vol.Ebs.DeleteOnTermination)
 		l = append(l, s)
 	}
 	return
